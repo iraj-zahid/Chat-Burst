80aa800979d51de5110f1a54db68882c90c017be
iraj <138902330+iraj-zahid@users.noreply.github.com>
2024-12-09 10:34:17 -0800

    Update route.js

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,9 +2,16 @@ import connectDB from "@/utils/connectDB"
 import Chat from "@/schema/chatSchema"
 import { NextResponse } from "next/server"
 
-export const POST = async (req, res) => {
-    const userChat = await req.json()
-    await connectDB()
-    await Chat.create(userChat)
-    return NextResponse.json({ chat: "submit" })
-}
+export const POST = async (req) => {
+    try {
+        const userChat = await req.json();
+        await connectDB();
+        const newChat = await Chat.create(userChat);
+        return NextResponse.json({ message: "Chat submitted successfully!", data: newChat });
+    } catch (error) {
+        return NextResponse.json(
+            { error: error.message || "Something went wrong!" },
+            { status: 400 }
+        );
+    }
+};
